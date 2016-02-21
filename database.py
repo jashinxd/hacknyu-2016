4f753f15a02545f5c298c449510085756a701382
jashinxd <shin.jason@gmail.com>
2016-02-21 15:16:34 +0000

    fixing string errot

--- database.py
+++ database.py
@@ -6,20 +6,20 @@ import md5
 def registerDentist(username,password,firstName,lastName,spec,cAddress,eAddress, pNum):
     required = [username, password, fistName, lastName, cAddress, pNum]
     connection = MongoClient()
-    db = connection['database']
+    db = connection["database"]
     newPass = str(md5.new(password).digest())
     for param in required:
         if param == "":
             return "missing"
-    db.dentists.insert({'uname': username, 'password': newPass, 'firstName': firstName,\
-    'lastName': lastName, 'spec': spec, 'cAddress': cAddress, 'eAddress': eAddress, 'pNum': pNum,\
-    'numPatients': 0,'patients': {'0':'null'}})
+    db.dentists.insert({"uname": username, "password": newPass, "firstName": firstName,\
+    "lastName": lastName, "spec": spec, "cAddress": cAddress, "eAddress": eAddress, "pNum": pNum,\
+    "numPatients": 0,"patients": {}})
 
 # Tests if the dentist's username is already registered or not
 def validDentistUname(username):
     connection = MongoClient()
-    db = connection['database']
-    curs = db.dentists.find({'uname':username})
+    db = connection["database"]
+    curs = db.dentists.find({"uname":username})
     if curs.count() == 0:
         return False
     return True
@@ -27,26 +27,26 @@ def validDentistUname(username):
 # Authentication for dentist login
 def authenticateDentist(username,password):
     connection = MongoClient()
-    db = connection['database']
+    db = connection["database"]
     #print username, password
     newPass = md5.new(password).digest()
-    curs = db.dentists.find({'uname':username, 'password':newPass})
+    curs = db.dentists.find({"uname":username, "password":newPass})
     #print curs.count()
     if curs.count() != 0:
         return True
     return False
 
-# Add a patient to dentist's current patient list
+# Add a patient to dentist"s current patient list
 def addPatient(dUsername, pFName, pLName, pUsername):
     connection = MongoClient()
-    db = connection['database']
-    currPatients = db.dentists.find({'uname': dUsername})[patients]
+    db = connection["database"]
+    currPatients = db.dentists.find({"uname": dUsername})[patients]
     if (pUsername != ""):
         newPatients = currPatients.append(pUsername)
     else:
-        pat = db.patients.find({'firstName': pFName, 'lastName': pLName}[uname])
+        pat = db.patients.find({"firstName": pFName, "lastName": pLName}[uname])
         newPatients = currPatients.append(pat)
-    db.dentists.update({'uname': dUsername}, {'patients': newPatients}, {upsert:true}) 
+    db.dentists.update({"uname": dUsername}, {"patients": newPatients}, {upsert:true}) 
     
 """
 # Insert picture name after upload using HTML/PHP
@@ -59,20 +59,20 @@ def newPicture(
 def registerPatient(username,password,firstName,lastName, hAddress, eAddress, pNum):
     required = [username, password, firstName, lastName, hAddress, pNum]
     connection = MongoClient()
-    db = connection['database']
+    db = connection["database"]
     newPass = str(md5.new(password).digest())
     for param in required:
         if param == "":
             return "missing"
-    db.patients.insert({'uname': username, 'password': newPass, 'firstName': firstName,\
-    'lastName': lastName, 'hAddress': hAddress, 'eAddress': eAddress, 'pNum': pNum,\
-    'numDentists': 0, 'dentists': {'0': 'null'}})
+    db.patients.insert({"uname": username, "password": newPass, "firstName": firstName,\
+    "lastName": lastName, "hAddress": hAddress, "eAddress": eAddress, "pNum": pNum,\
+    "numDentists": 0, "dentists": {}})
 
 # Tests if the patient's username is already registered or not    
 def validPatientUname(username):
     connection = MongoClient()
-    db = connection['database']
-    curs = db.patients.find({'uname':username})
+    db = connection["database"]
+    curs = db.patients.find({"uname":username})
    # print curs.count()
     if curs.count() == 0:
         return False
@@ -81,10 +81,10 @@ def validPatientUname(username):
 # Authentication for patient login
 def authenticatePatient(username,password):
     connection = MongoClient()
-    db = connection['database']
+    db = connection["database"]
     #print username, password
     newPass = md5.new(password).digest()
-    curs = db.patients.find({'uname':username, 'newPass':password})
+    curs = db.patients.find({"uname":username, "newPass":password})
     #print curs.count()
     if curs.count() != 0:
         return True
